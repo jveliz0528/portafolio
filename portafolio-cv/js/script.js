d92a8e4cfb6dce78f6e6aeb625420f2a0136dcdd
jveliz0528 <jveliz0528@gmail.com>
2021-05-25 17:15:16 -0300

    Ajuste al portafolio.

--- portafolio-cv/js/script.js
+++ portafolio-cv/js/script.js
@@ -27,7 +27,7 @@
   $form.addEventListener("submit", (e) => {
     e.preventDefault();
     $loader.classList.remove("none");
-    fetch("https://formsubmit.co/ajax/jveliz0528@gmail.com", {
+    fetch("https://formsubmit.co/ajax/jesus_abrahan0528@hotmail.com", {
       method: "POST",
       body: new FormData(e.target),
     })
